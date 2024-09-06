273fba4ac601166838da89eee94636be92e96163
Arnaud Van Acker <arnaudvanacker@yahoo.fr>
2024-09-06 11:42:48 +0200

    advanced enlevé

--- public/assets/js/components/nav.js
+++ public/assets/js/components/nav.js
@@ -1,39 +1,39 @@
 import { customGrid } from "./customGrid.js";
-import { advancedGrid } from "./advancedGrid.js";
+//import { advancedGrid } from "./advancedGrid.js"
 import { customGameOfLife } from "./customGameOfLife.js";
-import { advancedGameOfLife } from "./advancedGameOfLife.js";
+//import { advancedGameOfLife } from "./advancedGameOfLife.js"
 export function nav() {
     // CUSTOM / ADVANCED
-    const custom = document.querySelector('#customNav');
-    const advanced = document.querySelector('#advancedNav');
-    const customOptions = document.querySelector('#customOptions');
-    const advancedOptions = document.querySelector('#advancedOptions');
+    //const custom: HTMLDivElement = document.querySelector('#customNav')
+    //const advanced: HTMLDivElement = document.querySelector('#advancedNav')
+    //const customOptions: HTMLElement = document.querySelector('#customOptions')
+    //const advancedOptions: HTMLElement = document.querySelector('#advancedOptions')
     const customDisplay = document.querySelector('#displayCustom');
     const advancedDisplay = document.querySelector('#displayAdvanced');
     customDisplay.style.display = 'flex';
     advancedDisplay.style.display = 'none';
-    advancedOptions.style.display = 'none';
+    //advancedOptions.style.display = 'none'
     const grid = document.querySelector('#gridAdvanced');
     grid.innerHTML = '';
     clearAll();
-    custom.addEventListener('click', () => {
-        customDisplay.style.display = 'flex';
-        advancedDisplay.style.display = 'none';
-        advancedOptions.style.display = 'none';
-        customOptions.style.display = 'flex';
-        const grid = document.querySelector('#gridAdvanced');
-        grid.innerHTML = '';
-        clearAll();
-    });
-    advanced.addEventListener('click', () => {
-        customDisplay.style.display = 'none';
-        advancedDisplay.style.display = 'flex';
-        advancedOptions.style.display = 'flex';
-        customOptions.style.display = 'none';
-        clearAll();
-        const grid = document.querySelector('#gridCustom');
-        grid.innerHTML = '';
-    });
+    // custom.addEventListener('click', () => {
+    //     customDisplay.style.display = 'flex'
+    //     advancedDisplay.style.display = 'none'
+    //     advancedOptions.style.display = 'none'
+    //     customOptions.style.display = 'flex'
+    //     const grid: HTMLDivElement = document.querySelector('#gridAdvanced')!
+    //     grid.innerHTML = ''
+    //     clearAll()
+    // })
+    // advanced.addEventListener('click', () => {
+    //     customDisplay.style.display = 'none'
+    //     advancedDisplay.style.display = 'flex'
+    //     advancedOptions.style.display = 'flex'
+    //     customOptions.style.display = 'none'
+    //     clearAll()
+    //     const grid: HTMLDivElement = document.querySelector('#gridCustom')!
+    //     grid.innerHTML = ''
+    // })
     // CUSTOM START / STOP
     const startStop = document.querySelector('#customStartStop');
     startStop.addEventListener('click', () => {
@@ -56,37 +56,36 @@ export function nav() {
         clearAll();
     });
     // ADVANCED START / STOP
-    const advancedStartStop = document.querySelector('#advancedStartStop');
-    advancedStartStop.addEventListener('click', () => {
-        if (advancedStartStop.innerText == "Start") {
-            advancedStartStop.innerText = "Stop";
-            advancedGameOfLife();
-        }
-        else {
-            advancedStartStop.innerText = "Start";
-            localStorage.setItem('advancedRunning', 'false');
-        }
-    });
+    // const advancedStartStop: HTMLDivElement = document.querySelector('#advancedStartStop')
+    // advancedStartStop.addEventListener('click', () => {
+    //     if(advancedStartStop.innerText == "Start") {
+    //         advancedStartStop.innerText = "Stop"
+    //         advancedGameOfLife()
+    //     } else {
+    //         advancedStartStop.innerText = "Start"
+    //         localStorage.setItem('advancedRunning','false')
+    //     }
+    // })
     // ADVANCED CLEAR
-    const advancedClear = document.querySelector('#advancedClear');
-    advancedClear.addEventListener('click', () => {
-        if (advancedStartStop.innerText == "Stop") {
-            advancedStartStop.innerText = "Start";
-            localStorage.setItem('advancedRunning', 'false');
-        }
-        clearAll();
-    });
+    // const advancedClear: HTMLElement = document.querySelector('#advancedClear')
+    // advancedClear.addEventListener('click', () => {
+    //     if(advancedStartStop.innerText == "Stop") {
+    //         advancedStartStop.innerText = "Start"
+    //         localStorage.setItem('advancedRunning','false')
+    //     }
+    //     clearAll()
+    // })
 }
 function clearAll() {
     localStorage.setItem('customRunning', 'false');
     const customArray = [];
     localStorage.setItem('customArray', JSON.stringify(customArray));
-    const advancedArray = [];
-    localStorage.setItem('advancedArray', JSON.stringify(advancedArray));
+    // const advancedArray: Array<Array<number>> = []
+    // localStorage.setItem('advancedArray',JSON.stringify(advancedArray))
     let sizeInput = document.querySelector('#customSizeID');
     let value = sizeInput.value.split('x');
     customGrid(parseInt(value[0]), parseInt(value[1]));
-    sizeInput = document.querySelector('#advancedSizeID');
-    value = sizeInput.value.split('x');
-    advancedGrid(parseInt(value[0]), parseInt(value[1]));
+    // sizeInput = document.querySelector('#advancedSizeID')
+    // value = sizeInput.value.split('x')
+    // advancedGrid(parseInt(value[0]),parseInt(value[1]))
 }
--- src/ts/components/nav.ts
+++ src/ts/components/nav.ts
@@ -1,44 +1,44 @@
 import { customGrid } from "./customGrid.js"
-import { advancedGrid } from "./advancedGrid.js"
+//import { advancedGrid } from "./advancedGrid.js"
 import { customGameOfLife } from "./customGameOfLife.js"
-import { advancedGameOfLife } from "./advancedGameOfLife.js"
+//import { advancedGameOfLife } from "./advancedGameOfLife.js"
 
 export function nav(): void {
 
     // CUSTOM / ADVANCED
-    const custom: HTMLDivElement = document.querySelector('#customNav')
-    const advanced: HTMLDivElement = document.querySelector('#advancedNav')
-    const customOptions: HTMLElement = document.querySelector('#customOptions')
-    const advancedOptions: HTMLElement = document.querySelector('#advancedOptions')
+    //const custom: HTMLDivElement = document.querySelector('#customNav')
+    //const advanced: HTMLDivElement = document.querySelector('#advancedNav')
+    //const customOptions: HTMLElement = document.querySelector('#customOptions')
+    //const advancedOptions: HTMLElement = document.querySelector('#advancedOptions')
     const customDisplay: HTMLDivElement = document.querySelector('#displayCustom')
     const advancedDisplay: HTMLDivElement = document.querySelector('#displayAdvanced')
 
     customDisplay.style.display = 'flex'
     advancedDisplay.style.display = 'none'
-    advancedOptions.style.display = 'none'
+    //advancedOptions.style.display = 'none'
     const grid: HTMLDivElement = document.querySelector('#gridAdvanced')!
     grid.innerHTML = ''
     clearAll()
 
-    custom.addEventListener('click', () => {
-        customDisplay.style.display = 'flex'
-        advancedDisplay.style.display = 'none'
-        advancedOptions.style.display = 'none'
-        customOptions.style.display = 'flex'
-        const grid: HTMLDivElement = document.querySelector('#gridAdvanced')!
-        grid.innerHTML = ''
-        clearAll()
-    })
+    // custom.addEventListener('click', () => {
+    //     customDisplay.style.display = 'flex'
+    //     advancedDisplay.style.display = 'none'
+    //     advancedOptions.style.display = 'none'
+    //     customOptions.style.display = 'flex'
+    //     const grid: HTMLDivElement = document.querySelector('#gridAdvanced')!
+    //     grid.innerHTML = ''
+    //     clearAll()
+    // })
 
-    advanced.addEventListener('click', () => {
-        customDisplay.style.display = 'none'
-        advancedDisplay.style.display = 'flex'
-        advancedOptions.style.display = 'flex'
-        customOptions.style.display = 'none'
-        clearAll()
-        const grid: HTMLDivElement = document.querySelector('#gridCustom')!
-        grid.innerHTML = ''
-    })
+    // advanced.addEventListener('click', () => {
+    //     customDisplay.style.display = 'none'
+    //     advancedDisplay.style.display = 'flex'
+    //     advancedOptions.style.display = 'flex'
+    //     customOptions.style.display = 'none'
+    //     clearAll()
+    //     const grid: HTMLDivElement = document.querySelector('#gridCustom')!
+    //     grid.innerHTML = ''
+    // })
 
     // CUSTOM START / STOP
     const startStop: HTMLDivElement = document.querySelector('#customStartStop')
@@ -64,27 +64,27 @@ export function nav(): void {
     })
 
     // ADVANCED START / STOP
-    const advancedStartStop: HTMLDivElement = document.querySelector('#advancedStartStop')
+    // const advancedStartStop: HTMLDivElement = document.querySelector('#advancedStartStop')
 
-    advancedStartStop.addEventListener('click', () => {
-        if(advancedStartStop.innerText == "Start") {
-            advancedStartStop.innerText = "Stop"
-            advancedGameOfLife()
-        } else {
-            advancedStartStop.innerText = "Start"
-            localStorage.setItem('advancedRunning','false')
-        }
-    })
+    // advancedStartStop.addEventListener('click', () => {
+    //     if(advancedStartStop.innerText == "Start") {
+    //         advancedStartStop.innerText = "Stop"
+    //         advancedGameOfLife()
+    //     } else {
+    //         advancedStartStop.innerText = "Start"
+    //         localStorage.setItem('advancedRunning','false')
+    //     }
+    // })
 
     // ADVANCED CLEAR
-    const advancedClear: HTMLElement = document.querySelector('#advancedClear')
-    advancedClear.addEventListener('click', () => {
-        if(advancedStartStop.innerText == "Stop") {
-            advancedStartStop.innerText = "Start"
-            localStorage.setItem('advancedRunning','false')
-        }
-        clearAll()
-    })
+    // const advancedClear: HTMLElement = document.querySelector('#advancedClear')
+    // advancedClear.addEventListener('click', () => {
+    //     if(advancedStartStop.innerText == "Stop") {
+    //         advancedStartStop.innerText = "Start"
+    //         localStorage.setItem('advancedRunning','false')
+    //     }
+    //     clearAll()
+    // })
 }
 
 function clearAll() {
@@ -93,13 +93,13 @@ function clearAll() {
     const customArray: Array<Array<number>> = []
     localStorage.setItem('customArray',JSON.stringify(customArray))
 
-    const advancedArray: Array<Array<number>> = []
-    localStorage.setItem('advancedArray',JSON.stringify(advancedArray))
+    // const advancedArray: Array<Array<number>> = []
+    // localStorage.setItem('advancedArray',JSON.stringify(advancedArray))
 
     let sizeInput: HTMLSelectElement = document.querySelector('#customSizeID')
     let value: Array<string> = sizeInput.value.split('x')
     customGrid(parseInt(value[0]),parseInt(value[1]))
-    sizeInput = document.querySelector('#advancedSizeID')
-    value = sizeInput.value.split('x')
-    advancedGrid(parseInt(value[0]),parseInt(value[1]))
+    // sizeInput = document.querySelector('#advancedSizeID')
+    // value = sizeInput.value.split('x')
+    // advancedGrid(parseInt(value[0]),parseInt(value[1]))
 }
